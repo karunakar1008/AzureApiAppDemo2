d5324f03ed3a0dc305868d8f28622fc9cc289d06
karunakar Bhogyari <karunakar.bhogyari@kpmg.co.uk>
2022-03-11 14:20:45 +0000

    Add project files.

---
+++ DssDemoApiApp/Data/DssDemoApiAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DssDemoApiApp.Data
+{
+    public class DssDemoApiAppContext : DbContext
+    {
+        public DssDemoApiAppContext (DbContextOptions<DssDemoApiAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DssDemoApiApp.Data.Employee> Employee { get; set; }
+    }
+}
