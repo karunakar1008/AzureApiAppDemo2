023b223b06c425d4c2ef73e32e57f6d893c969d5
karunakar Bhogyari <karunakar.bhogyari@kpmg.co.uk>
2022-03-11 14:21:59 +0000

    Remove connection string

--- DssDemoApiApp/Program.cs
+++ DssDemoApiApp/Program.cs
@@ -13,7 +13,22 @@ namespace DssDemoApiApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<WebAPIDemoinCoreContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
