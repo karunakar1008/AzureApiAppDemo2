6f5de5cc646b4fad47802d97d860a5b15b1915c6
karunakar Bhogyari <karunakar.bhogyari@kpmg.co.uk>
2022-03-12 14:20:52 +0000

    Add client project

---
+++ ConsoleAppToCallAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleAppToCallAPI.Api;
+using ConsoleAppToCallAPI.Client;
+using System;
+
+namespace ConsoleAppToCallAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Configuration();
+            config.BasePath = "https://csnempapi.azurewebsites.net/";
+            EmployeesApi api = new EmployeesApi(config);
+            var emps = api.ApiEmployeesGet();
+
+            foreach (var emp in emps)
+            {
+                Console.WriteLine(emp.Name+" "+emp.Salary);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
