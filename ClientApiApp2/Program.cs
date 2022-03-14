8ce449cadda248951a0abd7405d0e0c550e92ed9
karunakar Bhogyari <karunakar.bhogyari@kpmg.co.uk>
2022-03-14 06:03:57 +0000

    Add client app2

--- ClientApiApp2/Program.cs
+++ ClientApiApp2/Program.cs
@@ -1,21 +1,21 @@
-﻿using ConsoleAppToCallAPI.Api;
-using ConsoleAppToCallAPI.Client;
+﻿using ClientApiApp2.Api;
+using ClientApiApp2.Client;
 using System;
 
-namespace ConsoleAppToCallAPI
+namespace ClientApiApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
             var config = new Configuration();
-            config.BasePath = "https://localhost:44329";
+            config.BasePath = "https://csnempapi.azurewebsites.net/";
             EmployeesApi api = new EmployeesApi(config);
             var emps = api.ApiEmployeesGet();
 
             foreach (var emp in emps)
             {
-                Console.WriteLine(emp.Name+" "+emp.Salary);
+                Console.WriteLine(emp.Name + " " + emp.Salary);
             }
             Console.WriteLine("Hello World!");
         }
